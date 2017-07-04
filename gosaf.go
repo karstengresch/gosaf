e0d528b96909b5dbed62be2fd31f5d7d12b9f0d2
Karsten Gresch <git-2015@by-doing.org>
2017-07-04 07:36:50 +0200

    Added Auth and Cookie.

--- gosaf.go
+++ gosaf.go
@@ -1,19 +1,46 @@
 package main
 
 import (
+	"encoding/base64"
 	"fmt"
 	"net/http"
+	"net/http/cookiejar"
 
 	"github.com/yhat/scrape"
 	"golang.org/x/net/html"
 	"golang.org/x/net/html/atom"
 )
 
+func basicAuth(username, password string) string {
+	auth := username + ":" + password
+	return base64.StdEncoding.EncodeToString([]byte(auth))
+}
+
+func redirectPolicyFunc(req *http.Request, via []*http.Request) error{
+	req.Header.Add("Authorization","Basic "+basicAuth("username1","password123"))
+	return nil
+}
+
+
+
 func main() {
-	resp, err := http.Get("https://news.ycombinator.com/")
+
+	cookieJar, _ := cookiejar.New(nil)
+
+	client := &http.Client{
+		Jar: cookieJar,
+		CheckRedirect: redirectPolicyFunc,
+	}
+
+	req, err := http.NewRequest("GET", "https://news.ycombinator.com/", nil)
+	req.SetBasicAuth("username1", "password123")
+
 	if err != nil {
 		panic(err)
 	}
+
+	resp, err := client.Do(req)
+
 	root, err := html.Parse(resp.Body)
 	if err != nil {
 		panic(err)
