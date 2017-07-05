d358e41e921d1850a01271c81da0d1c11e848da5
Karsten Gresch <git-2015@by-doing.org>
2017-07-05 18:24:13 +0200

    Still playing around w/ Cobra. Actually all I wanted were flags.

--- cmd/root.go
+++ cmd/root.go
@@ -27,13 +27,13 @@ var cfgFile string
 // RootCmd represents the base command when called without any subcommands
 var RootCmd = &cobra.Command{
 	Use:   "gosaf",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "A downloader for special ebooks",
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Long: `gosaf is a command line utility for scraping and packaging specific ebooks which cannot be downloaded.
+	Usage:
+	  -u=<username>
+	  -p=<passworkd>
+	  -i=<id of the book>`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	//	Run: func(cmd *cobra.Command, args []string) { },
@@ -58,7 +58,12 @@ func init() {
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	RootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	RootCmd.PersistentFlags().StringP("username", "u", "YOUR USERNAME", "Username of your S. account")
+	RootCmd.PersistentFlags().StringP("password", "p", "YOUR PASSWORD", "Password for your S. account")
+	RootCmd.PersistentFlags().StringP("bookid", "i", "THE BOOK ID", "Book ID. Look it up by opening the book in a web browser.")
+	viper.BindPFlag("username", RootCmd.PersistentFlags().Lookup("username"))
+	viper.BindPFlag("password", RootCmd.PersistentFlags().Lookup("password"))
+	viper.BindPFlag("bookid", RootCmd.PersistentFlags().Lookup("bookid"))
 }
 
 // initConfig reads in config file and ENV variables if set.
